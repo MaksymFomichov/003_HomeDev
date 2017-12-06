6dc2e54910d96c594ab81d3a83680ce1f6bbc2b3
MaksymFomichov <m.fomichov@gmail.com>
2017-12-06 17:46:09 +0200

    обновил сущность разрабочтика

--- src/main/java/com/fomichov/gmail/m/hibernate/model/Developer.java
+++ src/main/java/com/fomichov/gmail/m/hibernate/model/Developer.java
@@ -1,4 +1,51 @@
 package com.fomichov.gmail.m.hibernate.model;
 
+import java.math.BigDecimal;
+
 public class Developer {
+    private Long id;
+    private String name;
+    private BigDecimal salary;
+
+    public Developer() {
+    }
+
+    public Developer(Long id, String name, BigDecimal salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,56 +1,56 @@
-CREATE TABLE developers (
+CREATE TABLE IF NOT EXISTS developers (
   id   BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) UNIQUE NOT NULL
-);
+) ENGINE = InnoDB;
 
-CREATE TABLE skills (
+CREATE TABLE IF NOT EXISTSskills (
   id    BIGINT AUTO_INCREMENT PRIMARY KEY,
   skill VARCHAR(50) UNIQUE NOT NULL
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE projects (
+CREATE TABLE IF NOT EXISTSprojects (
   id      BIGINT AUTO_INCREMENT PRIMARY KEY,
   project VARCHAR(50) UNIQUE NOT NULL
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE companies (
+CREATE TABLE IF NOT EXISTScompanies (
   id      BIGINT AUTO_INCREMENT PRIMARY KEY,
   company VARCHAR(50) UNIQUE NOT NULL
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTScustomers (
   id       BIGINT AUTO_INCREMENT PRIMARY KEY,
   customer VARCHAR(50) UNIQUE NOT NULL
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE `developers_skills` (
+CREATE TABLE IF NOT EXISTS `developers_skills` (
   `developers_id` BIGINT,
   `skills_id`     BIGINT,
   PRIMARY KEY (`developers_id`, `skills_id`),
   CONSTRAINT fk_id_developer FOREIGN KEY (developers_id) REFERENCES developers (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_id_skill FOREIGN KEY (skills_id) REFERENCES skills (id) ON DELETE CASCADE ON UPDATE CASCADE
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE `developers_projects` (
+CREATE TABLE IF NOT EXISTS `developers_projects` (
   `developers_id` BIGINT,
   `projects_id`   BIGINT,
   PRIMARY KEY (`developers_id`, `projects_id`),
   CONSTRAINT fk_id_developer_project FOREIGN KEY (developers_id) REFERENCES developers(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_id_project FOREIGN KEY (projects_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE `companies_projects` (
+CREATE TABLE IF NOT EXISTS `companies_projects` (
   `companies_id` BIGINT,
   `projects_id`  BIGINT,
   PRIMARY KEY (`companies_id`, `projects_id`),
   CONSTRAINT fk_id_company FOREIGN KEY (companies_id) REFERENCES companies(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_id_project_company FOREIGN KEY (projects_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
+)ENGINE = InnoDB;
 
-CREATE TABLE `customers_projects` (
+CREATE TABLE IF NOT EXISTS `customers_projects` (
   `customers_id` BIGINT,
   `projects_id`  BIGINT,
   PRIMARY KEY (`customers_id`, `projects_id`),
   CONSTRAINT fk_id_customer FOREIGN KEY (customers_id) REFERENCES customers(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_id_project_customer FOREIGN KEY (projects_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
+)ENGINE = InnoDB;
